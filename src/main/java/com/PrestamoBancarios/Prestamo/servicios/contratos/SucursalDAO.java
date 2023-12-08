b2ffe41df9d315f9864d9f07ac89320f94a7021d
Oscar Menjívar <62222365+StevenMenjivar@users.noreply.github.com>
2023-12-08 17:13:14 -0600

    Delete src directory

--- src/main/java/com/PrestamoBancarios/Prestamo/servicios/contratos/SucursalDAO.java
+++
@@ -1,8 +0,0 @@
-package com.PrestamoBancarios.Prestamo.servicios.contratos;
-
-import com.PrestamoBancarios.Prestamo.modelo.entidades.Sucursal;
-
-public interface SucursalDAO extends GenericoDAO<Sucursal>{
-    Iterable<Sucursal> findSucursalByNombreSucursalContains(String nombreSucursal);
-    Iterable<Sucursal> findSucursalByNombreSucursalContainsIgnoreCase(String nombreSucursal);
-}
--- src/main/java/com/PrestamoBancarios/Prestamo/servicios/contratos/TipoClienteDAO.java
+++
@@ -1,8 +0,0 @@
-package com.PrestamoBancarios.Prestamo.servicios.contratos;
-
-import com.PrestamoBancarios.Prestamo.modelo.entidades.TipoCliente;
-
-public interface TipoClienteDAO extends GenericoDAO<TipoCliente>{
-    Iterable<TipoCliente> findTipoClienteByDescripcionTipoClienteContains(String descripcionTipoCliente);
-    Iterable<TipoCliente> findTipoClienteByDescripcionTipoClienteContainsIgnoreCase(String descripcionTipoCliente);
-}
